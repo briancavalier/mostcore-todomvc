1697a05f5594eae45b6863cce290fc811294355e
Brian Cavalier <brian@briancavalier.com>
2018-08-08 16:56:58 -0400

    Add composable interpreter sketch

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@
 // 1. localStorage
 // 2. todo editing
 /* global Document, Element */
-import { skipRepeats, map, merge, scan, tap, runEffects } from '@most/core'
+import { skipRepeats, map, scan, tap, runEffects } from '@most/core'
 import { newDefaultScheduler } from '@most/scheduler'
 import { hashchange } from '@most/dom-event'
+import { inject } from 'most-product'
 
 import { emptyApp } from './model'
 import { View } from './view.jsx'
 import { handleFilterChange } from './action'
-import { interpretTodos } from './interpret'
+import { interpretTodos, interpretFilterChange, composeInterpreters } from './interpret'
 import { createEventAdapter } from './eventAdapter'
 import * as ReactDOM from 'react-dom'
 
@@ -26,9 +27,11 @@ const [addAction, todoActions] = createEventAdapter(scheduler)
 
 const updateFilter = map(handleFilterChange, hashchange(window))
 
-const actions = merge(todoActions, updateFilter)
+const actions = inject(todoActions, updateFilter)
 
-const stateUpdates = skipRepeats(scan(interpretTodos, appState, actions))
+const interpret = composeInterpreters(interpretTodos, interpretFilterChange)
+
+const stateUpdates = skipRepeats(scan(interpret, appState, actions))
 const viewUpdates = tap(rel => ReactDOM.render(rel, appNode), map(View(addAction), stateUpdates))
 
 runEffects(viewUpdates, scheduler)
--- src/interpret.js
+++ src/interpret.js
@@ -1,6 +1,7 @@
 // @flow
 import type { Add, Complete, FilterChange, Input, Remove, RemoveAllCompleted, ToggleAll } from './action'
 import { type App, addTodo, setFilter, removeTodo, removeAllCompleted, updateAllCompleted, updateCompleted, updateInput } from './model'
+import { type Either } from 'most-product'
 
 // The types that this interpreter can interpret
 // TODO: modularize interpreters so we don't have
@@ -8,25 +9,30 @@ import { type App, addTodo, setFilter, removeTodo, removeAllCompleted, updateAll
 // possible to define interpreters over subsets of
 // these actions (even over a single action), and
 // compose them.
-type Action =
+type TodoAction =
   | Add
-  | Complete
-  | FilterChange
   | Input
+  | Complete
   | Remove
   | RemoveAllCompleted
   | ToggleAll
 
-export const interpretTodos = (app: App, action: Action): App => {
+export const interpretTodos = (app: App, action: TodoAction): App => {
   switch (action.action) {
     case 'input': return updateInput(action.input, app)
     case 'add': return addTodo(action.description, app)
     case 'complete': return updateCompleted(action.completed, action.todo, app)
-    case 'filterChange': return setFilter(action.filter, app)
     case 'remove': return removeTodo(action.todo, app)
     case 'removeAllCompleted': return removeAllCompleted(app)
     case 'toggleAll': return updateAllCompleted(action.completed, app)
+    default: return app
   }
-
-  return app
 }
+
+export const interpretFilterChange = (app: App, action: FilterChange): App =>
+  setFilter(action.filter, app)
+
+export type Interpreter<S, A> = (S, A) => S
+export const composeInterpreters = <S, A, B> (ia: Interpreter<S, A>, ib: Interpreter<S, B>): Interpreter<S, Either<A, B>> =>
+  (s: S, eab: Either<A, B>): S =>
+    eab.right ? ib(s, eab.value) : ia(s, eab.value)
