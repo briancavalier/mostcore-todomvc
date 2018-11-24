95b6bc00d3463e4c76c5d9fc7a2e643ef27457be
Brian Cavalier <brian@briancavalier.com>
2018-11-23 22:07:46 -0500

    Remove unused eventAdapter

--- src/eventAdapter.js
+++
@@ -1,8 +0,0 @@
-// @flow
-import type { Scheduler, Stream } from '@most/types'
-import { MulticastSource, never } from '@most/core'
-
-export const createEventAdapter = <A>(scheduler: Scheduler): ([A => void, Stream<A>]) => {
-  const stream = new MulticastSource(never())
-  return [action => stream.event(scheduler.currentTime(), action), stream]
-}
